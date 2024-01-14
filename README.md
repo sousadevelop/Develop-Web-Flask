8fb563dd08f417a0c90b33267586d6ed5714b781
João Victor <92130316+sousadevelop@users.noreply.github.com>
2024-01-14 00:01:37 -0300

    Criado e integrado a aplicação ao DB

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Develop-Web-Flask
  Este aplicativo web é criado em Python usando o framework Flask. O Projeto será algo generico inicialmente para ser uma base para escalar e otimizar ele, afim de treinar mais o conhecimento em Flask.
 
+<p align="left">
+	<a href="#"><img src="https://img.shields.io/badge/made%20with-Python-green"></a>
+	<a href="#"><img src="https://img.shields.io/badge/made%20with-Flask-peacock"></a>
+</p>
+
 ## Forma de uso do projeto
  Primeiro clone o projeto.
  Segundo entre em um ambiente virtual usando o comando:
--- board/__init__.py
+++ board/__init__.py
@@ -1,11 +1,23 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
-from board import pages
+from board import pages, posts
+from board import pages, posts, database
+
+load_dotenv()
 
 # Essa função é a fábrica de aplicativos
 def create_app():
     app = Flask(__name__)
+    app.config.from_prefixed_env()
+
+    database.init_app(app
+                       )
 
     app.register_blueprint(pages.bp)
+    app.register_blueprint(posts.bp)
+    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")  # os.getenv -> usado para var em que não comecem com FLASK
+    print(f"Using Database: {app.config.get('DATABASE')}")     # app.config.get -> usado para var que começam com FLASK
     return app
 
 if __name__ == "__main__":
